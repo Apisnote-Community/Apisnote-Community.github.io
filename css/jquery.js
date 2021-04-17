62f3d84d9f617d3a403eee0ef180e4e4d486ba9a
t-takizawa77 <77kagawa@gmail.com>
2021-04-17 14:21:04 +0900

    1-1 version of io

---
+++ css/jquery.js
@@ -0,0 +1,27 @@
+﻿<script>
+
+$(function(){
+
+var header = $('header-5')
+
+header_offset = header.offset();
+
+header_height = header.height();
+
+$(window).scroll(function () {
+
+if($(window).scrollTop() > header_offset.top + header_height) {
+
+header.addClass('scroll');
+
+}else {
+
+header.removeClass('scroll');
+
+}
+
+});
+
+});
+
+</script>
