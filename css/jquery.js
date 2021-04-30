aa2b2d88852493b9eacc97be74558cb99854cab7
t-takizawa77 <77kagawa@gmail.com>
2021-04-30 17:57:10 +0900

    1-6 version

--- css/jquery.js
+++ css/jquery.js
@@ -2,12 +2,17 @@
 
 $(window).scroll(function(){
   var top = $(window).scrollTop();
-  if ( top > 2 ){
+  if ( top > 5 ){
     $('.header-5').removeClass('top');
     $('.header-5').addClass('scroll');
+    $('.header-nav-item').addClass('scroll');
+    $('.header-nav-item:hover').addClass('scroll');
+    $('.logo_img').attr('src',"image/apisnote-logo.png");
     }
   else{
     $('.header-5').removeClass('scroll');
     $('.header-5').addClass('top');
+    $('.header-nav-item').removeClass('scroll');
+    $('.logo_img').attr('src',"image/apisnote_logo.png");
    }
 });
